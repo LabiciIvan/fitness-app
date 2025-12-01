bc9c470e42c3f7cbb62164a99d102939fbafee47
Labici Ioan <ioanlabici@gmail.com>
2025-12-01 13:18:43 +0200

    FA-1: Profile model.

---
+++ app/Models/Profile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Profile extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProfileFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'sex',
+        'description',
+        'country',
+        'city',
+        'phone',
+        'logo',
+        'complete',
+    ];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+}
