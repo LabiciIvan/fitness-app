1c0c1299a40dbf23ecf5eea1ed53839210e36a29
Labici Ioan <ioanlabici@gmail.com>
2025-12-02 07:13:10 +0200

    FA-1: Configuration for tables.

---
+++ config/tables.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'types' => [
+        'customerKey' => 'customer',
+        'trainerKey' => 'trainer',
+        'title' => ['trainer', 'customer']
+    ],
+    'profiles' => [
+        'sex' => ['male', 'female']
+    ],
+    'programs' => [
+        'CRUD' => [
+            'create' => 'Create program'
+        ],
+        'difficulty' => ['beginner', 'intermediate', 'advanced']
+    ]
+];
