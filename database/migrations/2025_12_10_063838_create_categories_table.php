e8fc60479937f379e5cb6c611917d63bdabbbe0f
Labici Ioan <ioanlabici@gmail.com>
2025-12-24 11:45:39 +0200

    FA-1: Migrations updates.

---
+++ database/migrations/2025_12_10_063838_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title')->unique();
+            $table->string('description');
+        });
+
+        Schema::create('categories_programs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('program_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('categories_programs');
+    }
+};
