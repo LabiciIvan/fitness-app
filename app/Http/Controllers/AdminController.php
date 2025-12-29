62bded921929ef2e9e67941b92cc17df81dc8339
Labici Ioan <ioanlabici@gmail.com>
2025-12-29 07:48:32 +0200

    FA-1: Admin controller to handle resources.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use App\Http\Controllers\TagController;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class AdminController extends Controller
 {
@@ -15,22 +17,136 @@ class AdminController extends Controller
         'categories' => CategoriesController::class,
     ];
 
+    private array $requestsCreate = [
+        'tags'        => [
+            'tag' => 'required|unique:tags,tag|max:100'
+        ],
+        'types'       => [
+            'title'       => 'required|max:150|unique:types,title',
+        ],
+        'users'       => [
+            'name'     => 'required|max:200',
+            'email'    => 'required|email|unique:users,email',
+            'password' => 'required',
+
+        ],
+        'categories'  => [
+            'title'       => 'required|max:150|unique:categories,title',
+            'description' => 'required|max:250',
+        ],
+    ];
+
+    private array $requestsUpdate = [
+        'tags'        => [
+            'tag' => 'required|unique:tags,tag|max:100'
+        ],
+        'types'       => [
+            'title'       => 'required|max:150|unique:types,title',
+        ],
+        'users'       => [
+            'name'     => 'required|max:200',
+        ],
+        'categories'  => [
+            'title'       => 'required|max:150|unique:categories,title',
+            'description' => 'required|max:250',
+        ],
+    ];
+
     public function index() {
         return view('admin.index');
     }
 
-    public function callControllerAction(mixed $controller, string $action) {
+    public function callControllerMethod(mixed $controller, string $method, ?string $id = null) {
         if (!isset($this->maps[$controller])) {
             abort(404);
         }
 
         $instance = App::make($this->maps[$controller]);
 
-        if (!method_exists($instance, $action)) {
+        if (!method_exists($instance, $method)) {
             abort(404);
         }
 
-        return App::call([$instance, $action]);
+        return App::call([$instance, $method], [
+            'id' => $id
+        ]);
+    }
+
+    public function callControllerMethodForPost(Request $request, mixed $controller, string $method, ?string $id = null) {
+        if (!isset($this->requests[$controller], $this->maps[$controller])) {
+            abort(404);
+        }
+
+        $controllerInstance = App::make($this->maps[$controller]);
+
+        if (!method_exists($controllerInstance, $method)) {
+            abort(404);
+        }
+
+        $validationRules = $this->requestsCreate[$controller];
+
+        $validator = Validator::make($request->all(), $validationRules);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->route('admin.callControllerMethod', ['controller' => $controller, 'method' => 'create'])
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $validated = $validator->validated();
+
+        // Start collecting validated fileds in a form of associate array,
+        // where keys are the columns of coresponding model
+        // and pass it to controller store() method.
+        $data = [];
+
+        foreach ($validationRules as $field => $rule) {
+            $data[$field] = $validated[$field];
+        }
+
+        return App::call([$controllerInstance, $method], [
+            'data' => $data
+        ]);
+    }
+
+    public function callControllerMethodForPatch(Request $request, mixed $controller, string $method, string $id) {
+        if (!isset($this->requestsUpdate[$controller], $this->maps[$controller])) {
+            abort(404);
+        }
+
+        $controllerInstance = App::make($this->maps[$controller]);
+
+        if (!method_exists($controllerInstance, $method)) {
+            abort(404);
+        }
+
+        $validationRules = $this->requestsUpdate[$controller];
+
+        $validator = Validator::make($request->all(), $validationRules);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->route('admin.callControllerMethod', ['controller' => $controller, 'method' => 'create'])
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $validated = $validator->validated();
+
+        // Start collecting validated fileds in a form of associate array,
+        // where keys are the columns of coresponding model
+        // and pass it to controller store() method.
+        $data = [];
+
+        foreach ($validationRules as $field => $rule) {
+            $data[$field] = $validated[$field];
+        }
+
+        return App::call([$controllerInstance, $method], [
+            'data' => $data,
+            'id'   => $id
+        ]);
     }
 
 }
