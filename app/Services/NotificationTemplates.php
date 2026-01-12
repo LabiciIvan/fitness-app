ab4c0323caf17f5808fc5321fbb49b8ed11493c3
unknown <ioanlabici@gmail.com>
2026-01-13 00:09:16 +0200

    FA-1: Notification template.

---
+++ app/Services/NotificationTemplates.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+class NotificationTemplates {
+
+    public const TEMPLATES = [
+        'program.enrolled' => [
+            'message' => ':user enrolled in your program ":program".',
+            'route'   => 'programs.show',
+        ],
+
+        'program.unsubscribed' => [
+            'message' => ':user unsubscribed from your program ":program".',
+            'route'   => 'programs.show',
+        ],
+
+        'program.reminder' => [
+            'message' => 'Your enrollment in ":program" starts today.',
+            'route'   => 'programs.show',
+        ],
+
+        'trainer.followed' => [
+            'message' => ':user started following you.',
+            'route'   => 'trainers.show',
+        ],
+    ];
+}
