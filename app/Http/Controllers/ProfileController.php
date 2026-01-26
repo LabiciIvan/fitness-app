efaac0a33d362ec475a013ac0895cb7812b2ba61
unknown <ioanlabici@gmail.com>
2026-01-26 09:32:16 +0200

    FA-1: Profile controller updates.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RequestStoreProfile;
 use App\Models\Profile;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
     public function create() {
         return view('profile.create');
+    }
+
+    public function show(Profile $profile) {
+
+        return view('profile.show', [
+            'user' => $this->user,
+            'profile' => $profile
+        ]);
     }
 
     public function storeOrUpdate(RequestStoreProfile $request) {
