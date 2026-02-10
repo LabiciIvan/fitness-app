de87bd0cc786d4c51df87b3c047f21e8070d1d5a
unknown <ioanlabici@gmail.com>
2026-02-10 10:20:09 +0200

    FA-1: Update and modification in App Service Provider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('is-trainer', function (User $user) {
+            return $user->types->contains('title', config('tables.types.trainerKey'));
+        });
+
+        Gate::define('is-customer', function (User $user) {
+            return $user->types->contains('title', config('tables.types.customerKey'));
+        });
+
+        Gate::define('is-admin', function (User $user) {
+            return $user->types->contains('title', config('tables.types.adminKey'));
+        });
+
+        View::composer('*', function ($view) {
+            /** @var User|null $user */
+            $user = Auth::user();
+
+            $notificationsCount = $user ? $user->unreadNotifications()->count() : 0;
+
+            $view->with('unreadNotificationsCount', $notificationsCount);
+        });
     }
 }
